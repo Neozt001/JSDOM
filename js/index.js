6f19041446f4532de8a7d47415e203e61485ef58
Necati Öztek <neo001@edu.zealand.dk>
2026-09-22 10:22:23 +0200

    Add Task 1

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
 //Write your Javascript code here
 console.log("Shoppinglist")
+//Task 1
+function createElement(id, textnode, atr){
+    let newElement = document.createElement("li");
+    newElement.setAttribute("id", id);
+    newElement.setAttribute("class", atr);
+    newTextNode = document.createTextNode(textnode);
+    newElement.appendChild(newTextNode);
+    
+    return newElement;
+
+}
+
+// let list = document.getElementById("list");
+// list.appendChild(addElement("Coke", "unhealthy"))
+let e = createElement("", "COLA", "healthy")
+console.log(e)
+console.log("Shoppinglist End")
